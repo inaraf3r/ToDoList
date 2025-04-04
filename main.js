546ca75fd573335d8a7883b78c28d1a38e077e14
Nara <inarafernanda2358@gmail.com>
2025-04-04 20:21:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let botao = document.getElementById('btn-add');
+let tarefa = document.getElementById('input-tarefa');
+let lista = document.getElementById('list');
+
+botao.addEventListener('click', function(){
+    let paragrafo = document.createElement('p');
+    paragrafo.innerText = tarefa.value;
+    paragrafo.classList.add('estilo-paragrafo');
+    lista.appendChild(paragrafo);
+
+    paragrafo.addEventListener('click', function(){
+        paragrafo.style.textDecoration = 'line-through';
+
+     paragrafo.addEventListener('dblclick', function(){
+            lista.removeChild(paragrafo);
+
+})
+    })
+})
